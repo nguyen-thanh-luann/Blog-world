681451dc71f44620c7c108297fc2bdfcd294c9a3
nguyen-thanh-luann <imthanhluan@gmail.com>
2022-08-11 17:29:03 +0700

    get articles from firebase

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
-export default function Article() {
-  return <div>Article</div>
+export default function Article({ article }) {
+  return (
+    <div className="border-dotted border-2 border-sky-500 p-2">
+      <h2>{article.title}</h2>
+    </div>
+  )
 }
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,15 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import { db } from '../firebaseConfig'
+import { collection, query, orderBy, onSnapshot } from 'firebase/firestore'
+
+import Loading from '../components/Loading'
 import Header from '../components/Header'
 import Article from '../components/Article'
 import AddArticle from '../components/AddArticle'
 export default function HomeScreen() {
+  const [articles, setArticles] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const articleRef = collection(db, 'Articles')
+    const q = query(articleRef, orderBy('createdAt', 'desc'))
+    onSnapshot(q, (snapshot) => {
+      console.log(snapshot.docs)
+      const articles = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }))
+      setArticles(articles)
+      setIsLoading(false)
+    })
+  }, [])
+
   return (
     <div>
       <Header />
-      <div className='container px-20 py-5'>
-        <div className='grid grid-cols-3 '>
-          <div className='col-span-2'>
-            <Article />
+      <div className="container px-20 py-5">
+        <div className="grid grid-cols-3 ">
+          <div className="col-span-2 px-40">
+            {isLoading ? (
+              <div>
+                <Loading />
+              </div>
+            ) : articles.length === 0 ? (
+              <div>
+                <h3>No Article Found</h3>
+              </div>
+            ) : (
+              articles &&
+              articles.map((article) => (
+                <Article key={article.id} article={article} />
+              ))
+            )}
           </div>
           <div>
             <AddArticle />
